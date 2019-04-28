d762b84a6d45ef17f53664d097729cb4c04b04f2
Vaibhav Vachhani <vaibhav.vachhani@hotmail.com>
2019-04-28 16:03:14 +1000

    dayOfProgrammer
    tested and submitted

---
+++ dayOfProgrammer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string dayOfProgrammer(int year)
+        {
+            //if 
+            if (year == 1918)
+            {
+                return ("26.09.1918");
+            }
+            else if ( year < 1918)
+            {
+                if (year % 4 == 0)
+                {
+                    return ("12.09." + year.ToString());
+                }
+                else
+                {
+                    return ("13.09." + year.ToString());
+                }
+            }
+            else
+            {
+                if(year%400==0 ||  (year % 4 == 0 && year % 100 != 0))
+                {
+                   
+                    {
+                        return ("12.09." + year.ToString());
+                    }
+                }
+                else
+                {
+                    return ("13.09." + year.ToString());
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string y1 = dayOfProgrammer(2016);
+            Console.WriteLine(y1);
+        }
+    }
+}
